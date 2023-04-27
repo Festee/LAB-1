ba3facf7adbb8362891847f28f4d437d6ec4fbc5
Festee <96444399+Festee@users.noreply.github.com>
2023-04-28 00:30:55 +0200

    STRUKTURA

---
+++ drejtori-lista-orare.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="drejtori-lista-orare.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E/%22%3E">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <button><a href="drejtori-faqjakryesore.php"><i class="fa-solid fa-backward"></i></a></button>
+    <div class="oraret">
+      <div class="first">
+        <input type="text"  id="klase" placeholder="Klasa...">
+        <table class="my-table"> 
+            <thead>
+              <tr>
+                <th></th>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+                <th>4</th>
+                <th>5</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td id="dita">E hene</td>
+                <td id="one"><input type="text" id="one"></td>
+                <td id="two"><input type="text" id="one"></td>
+                <td id="three"><input type="text" id="one"></td>
+                <td id="four"><input type="text" id="one"></td>
+                <td id="five"><input type="text" id="one"></td>
+              </tr>
+              <tr>
+                <td id="dita">E marte</td>
+                <td id="one"><input type="text" id="one"></td>
+                <td id="two"><input type="text" id="one"></td>
+                <td id="three"><input type="text" id="one"></td>
+                <td id="four"><input type="text" id="one"></td>
+                <td id="five"><input type="text" id="one"></td>
+              </tr>
+              <tr>
+                <td id="dita">E merkure</td>
+                <td id="one"><input type="text" id="one"></td>
+                <td id="two"><input type="text" id="one"></td>
+                <td id="three"><input type="text" id="one"></td>
+                <td id="four"><input type="text" id="one"></td>
+                <td id="five"><input type="text" id="one"></td>
+              </tr>
+              <tr>
+                <td id="dita">E enjte</td>
+                <td id="one"><input type="text" id="one"></td>
+                <td id="two"><input type="text" id="one"></td>
+                <td id="three"><input type="text" id="one"></td>
+                <td id="four"><input type="text" id="one"></td>
+                <td id="five"><input type="text" id="one"></td>
+              </tr>
+              <tr>
+                <td id="dita">E premte</td>
+                <td id="one"><input type="text" id="one"></td>
+                <td id="two"><input type="text" id="one"></td>
+                <td id="three"><input type="text" id="one"></td>
+                <td id="four"><input type="text" id="one"></td>
+                <td id="five"><input type="text" id="one"></td>
+              </tr>
+            </tbody>
+          </table>         
+          <div class="butonat">
+            <td><button id="update-btn"><i class="fa-solid fa-arrows-rotate"></i></button></td>
+            <td><button id="delete-btn"><i class="fa-solid fa-trash"></i></button></td>   
+          </div>  
+    </div>          
+</div>
+</body>
+</html>
