ec0fc2b1e74dc753cd84abf74829045b62aee45c
puhizaa <121295219+puhizaa@users.noreply.github.com>
2023-05-05 22:03:01 +0200

    CRUD - MESUESI

---
+++ DREJTORI/drejtori-lista-mesues.php
@@ -0,0 +1,100 @@
+<?php
+    $db=mysqli_connect("localhost","root","","lab-1");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mesuesit</title>
+    <link rel="stylesheet" href="drejtori-lista-mesues.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <button id="back"><a href="drejtori-faqjakryesore.php"><i class="fa-solid fa-backward"></i></a></button>
+    <div class="djathtas">
+        <h1 id="lista">Lista e mësuesve</h1>
+        <div class="search-bar">
+            <input type="text" id="search" placeholder="search here...">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div class="lista">
+        <?php
+                    $qry="SELECT * from mesuesit";
+                    $run=$db->query($qry);
+                    if($run-> num_rows>0){
+                        while($row = $run -> fetch_assoc()){
+                            $id=$row['ID'];
+                            $mesuesi_id=$row['Mesuesi_ID'];
+                            $emri=$row['Emri'];
+                            $mbiemri=$row['Mbiemri'];
+                            $email=$row['Email'];
+                            $telefoni=$row['NrTel'];
+                            $gjinia=$row['Gjinia'];
+                            $datelindja=$row['Datelindja'];
+                            $adresa=$row['Adresa'];
+                            $login_email=$row['LoginEmail'];
+                            $login_password=$row['LoginPassword'];
+                            $klasa=$row['Klasa'];
+                ?>
+            <table id="mesues">
+               
+                <tr>
+                  <td>ID:</td>
+                  <td style="color: red; text-decoration: underline;"><?php echo $row['Mesuesi_ID']?></td>
+                </tr>
+                <tr>
+                    <td>EMRI:</td>
+                    <td style="color: red; text-decoration: underline;"><?php echo $row['Emri']?></td>
+                </tr>
+                <tr>
+                    <td>MBIEMRI:</td>
+                    <td style="color: red; text-decoration: underline;"><?php echo $row['Mbiemri']?></td>
+                </tr>
+                <tr>
+                    <td>EMAIL:</td>
+                    <td style="color: red; text-decoration: underline;"><?php echo $row['Email']?></td>
+                </tr>
+                <tr>
+                    <td>NR.TEL:</td>
+                    <td style="color: red; text-decoration: underline;"><?php echo $row['NrTel']?></td>
+                </tr>
+                <tr>
+                    <td>GJINIA:</td>
+                    <td style="color: red; text-decoration: underline;"><?php echo $row['Gjinia']?></td>
+                </tr>
+                <tr>
+                    <td>DATELINDJA:</td>
+                    <td style="color: red; text-decoration: underline;"><?php echo $row['Datelindja']?></td>
+                </tr>
+                <tr>
+                    <td>ADRESA:</td>
+                    <td style="color: red; text-decoration: underline;"><?php echo $row['Adresa']?></td>
+                </tr>
+                <tr>
+                    <td>LOGIN EMAIL:</td>
+                    <td style="color: red; text-decoration: underline;"><?php echo $row['LoginEmail']?></td>
+                </tr>
+                <tr>
+                    <td>LOGIN PASS:</td>
+                    <td style="color: red; text-decoration: underline;"><?php echo $row['LoginPassword']?></td>
+                </tr>
+                <tr>
+                  <td>Klasa:</td>
+                  <td style="color: red; text-decoration: underline;"><?php echo $row['Klasa']?></td>
+                </tr>
+                <tr>
+                    <td><a href="drejtori-edit-mesuesi.php?id=<?php echo $id;?>"><button id="update-btn"><i class="fa-solid fa-arrows-rotate"></i></button></a></td>
+                    <td><a href="delete-mesuesi.php?id=<?php echo $id;?>"><button id="delete-btn"><i class="fa-solid fa-trash"></i></button></a></td>
+                </tr>
+                <?php
+                   }
+                    }
+                ?>
+              </table>
+        </div>
+    </div>
+</body>
+</html>
