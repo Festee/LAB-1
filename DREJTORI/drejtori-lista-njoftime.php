e6ffc419bd39309847f0bec824e8e197c1504c77
FestimZylfiu <121205773+FestimZylfiu@users.noreply.github.com>
2023-06-14 00:33:48 +0200

    CRUD - NJOFTIME

--- DREJTORI/drejtori-lista-njoftime.php
+++ DREJTORI/drejtori-lista-njoftime.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mesuesit</title>
+    <title>DREJTORI LISTA E NJOFTIMEVE</title>
     <link rel="stylesheet" href="drejtori-lista-njoftime.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -49,5 +49,26 @@
 
         </div>
     </div>
+    <script>
+        document.getElementById("search").addEventListener("input", function () {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("search");
+            filter = input.value.toUpperCase();
+            table = document.querySelector("table");
+            tr = table.getElementsByTagName("tr");
+
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[0];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        });
+    </script>
 </body>
 </html>
